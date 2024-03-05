ae17d37173b5861a153f8896699c2165992dbf6a
kellyDMmendes <152336851+kellyDMmendes@users.noreply.github.com>
2024-03-05 19:43:31 -0300

    Upgrade 1005.py

--- 1005.py
+++ 1005.py
@@ -1,5 +1,5 @@
 #1005 - Média 1
-nt1 = float(input())
-nt2 = float(input())
-media = ((nt1*3.5)+(nt2*7.5))/11
+a = float(input())
+b = float(input())
+media = ((a * 3.5) + (b * 7.5)) / 11
 print("MEDIA = {0:.5f}".format(media))
