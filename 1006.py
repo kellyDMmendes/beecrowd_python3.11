610b67e93d25d132179aec1ba4bd0f9bc718156a
kellyDMmendes <kellymendes169@gmail.com>
2024-03-12 18:31:09 -0300

    Update 1006.py

--- 1006.py
+++ 1006.py
@@ -1,8 +1,8 @@
-#1006 - Média 2
+# 1006 - Média 2
 a = float(input())
 b = float(input())
 c = float(input())
 
 media = (a * 2 + b * 3 + c * 5) / 10
 
-print(f'MEDIA = {media:.1f}')
+print(f"MEDIA = {media:.1f}")
