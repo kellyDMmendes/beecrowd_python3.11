f87afa0bb982aa4d35098295630d0d037edfe016
kellyDMmendes <kellymendes169@gmail.com>
2024-03-12 18:31:16 -0300

    Update 1009.py

--- 1009.py
+++ 1009.py
@@ -1,8 +1,8 @@
-#1009 - Salário com Bônus
+# 1009 - Salário com Bônus
 nome = input()
 salario = float(input())
 vendas = float(input())
 
 total = salario + vendas * 15 / 100
 
-print(f'TOTAL = R$ {total:.2f}')
+print(f"TOTAL = R$ {total:.2f}")
