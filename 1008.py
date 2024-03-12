4d4911a0e20b72babfed102d0265429762b5b0a1
kellyDMmendes <kellymendes169@gmail.com>
2024-03-12 18:31:14 -0300

    Update 1008.py

--- 1008.py
+++ 1008.py
@@ -1,9 +1,9 @@
-#1008 - Salário
+# 1008 - Salário
 n = int(input())
 h = int(input())
 vh = float(input())
 
 sal = h * vh
 
-print(f'NUMBER = {n}')
-print(f'SALARY = U$ {sal:.2f}')
+print(f"NUMBER = {n}")
+print(f"SALARY = U$ {sal:.2f}")
