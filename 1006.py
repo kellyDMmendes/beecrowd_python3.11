5e55b08fac060e07a2afd3509f67e236bcd2e0e7
kellyDMmendes <152336851+kellyDMmendes@users.noreply.github.com>
2024-03-12 16:19:38 -0300

    Update 1006.py

--- 1006.py
+++ 1006.py
@@ -2,5 +2,7 @@
 a = float(input())
 b = float(input())
 c = float(input())
-media = ((a * 2) + (b * 3) + (c * 5)) / 10
-print("MEDIA = {0:.1f}".format(media))
+
+media = (a * 2 + b * 3 + c * 5) / 10
+
+print(f'MEDIA = {media:.1f}')
