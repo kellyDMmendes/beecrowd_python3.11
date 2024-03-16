f4a76318523f1a2fdb320667129ef6cbf307f86c
kellyDMmendes <kellymendes169@gmail.com>
2024-03-16 19:52:12 -0300

    Create b_1015.py

---
+++ b_1015.py
@@ -0,0 +1,13 @@
+""" b_1015 - Distância Entre Dois Pontos """
+from math import sqrt
+
+x1, y1 = input().split()
+x2, y2 = input().split()
+x1 = float(x1)
+y1 = float(y1)
+x2 = float(x2)
+y2 = float(y2)
+
+distancia = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+print(f"{distancia:.4f}")
