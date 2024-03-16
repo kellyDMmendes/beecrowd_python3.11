31c03626af2d6dd0fa219583f1c259ef26a019e3
kellyDMmendes <kellymendes169@gmail.com>
2024-03-16 18:49:51 -0300

    Update b_1010.py

--- b_1010.py
+++ b_1010.py
@@ -4,10 +4,12 @@ VALOR = 0
 
 while CONT < 2:
     cod, numPecas, valUnit = input().split()
+    cod = int(cod)
+    numPecas = int(numPecas)
+    valUnit = float(valUnit)
 
-    valor = int(numPecas) * float(valUnit)
+    valor = numPecas * valUnit
     VALOR += valor
-
     CONT += 1
 
 print(f"VALOR A PAGAR: R$ {VALOR:.2f}")
