1f6e8ca9ce3f5b53c2dab57b2d7230f0634ee0c9
kellyDMmendes <kellymendes169@gmail.com>
2024-03-17 20:05:07 -0300

    Create b_1047.py

---
+++ b_1047.py
@@ -0,0 +1,19 @@
+""" b_1047 - Tempo de Jogo com Minutos """
+hi, mi, hf, mf = input().split()
+hi = int(hi)
+mi = int(mi)
+hf = int(hf)
+mf = int(mf)
+
+if mi <= mf:
+    minutos = mf - mi
+else:
+    minutos = (60 - mi) + mf
+    hf -= 1
+
+if hi <= hf and mi != mf:
+    horas = hf - hi
+else:
+    horas = (24 - hi) + hf
+
+print(f"O JOGO DUROU {horas} HORA(S) E {minutos} MINUTO(S)")
