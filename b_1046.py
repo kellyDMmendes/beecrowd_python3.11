bc9321d82474c2840fa7325f9ffde42ae21307a5
kellyDMmendes <kellymendes169@gmail.com>
2024-03-17 19:02:22 -0300

    Create b_1046.py

---
+++ b_1046.py
@@ -0,0 +1,11 @@
+""" b_1046 - Tempo de Jogo """
+hi, hf = input().split()
+hi = int(hi)
+hf = int(hf)
+
+if hi < hf:
+    duracao = hf - hi
+    print(f"O JOGO DUROU {duracao} HORA(S)")
+else:
+    duracao = (24 - hi) + hf
+    print(f"O JOGO DUROU {duracao} HORA(S)")
