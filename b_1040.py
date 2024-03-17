618c1b983965ca7b7862e5a8171f708f8d8a6f12
kellyDMmendes <kellymendes169@gmail.com>
2024-03-17 16:52:01 -0300

    Create b_1040.py

---
+++ b_1040.py
@@ -0,0 +1,24 @@
+""" b_1040 - Média 3 """
+n1, n2, n3, n4 = input().split()
+n1 = float(n1)
+n2 = float(n2)
+n3 = float(n3)
+n4 = float(n4)
+
+media = (n1 * 2 + n2 * 3 + n3 * 4 + n4) / 10
+print(f"Media: {media:.1f}")
+
+if media < 5:
+    print("Aluno reprovado.")
+elif media >= 7:
+    print("Aluno aprovado.")
+else:
+    print("Aluno em exame.")
+    n = float(input())
+    print(f"Nota do exame: {n:.1f}")
+    media = (media + n) / 2
+    if media < 5:
+        print("Aluno reprovado.")
+    else:
+        print("Aluno aprovado.")
+    print(f"Media final: {media:.1f}")
