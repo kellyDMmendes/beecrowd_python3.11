375610a8765c75cbefb799341fbfa5b6a005d1e1
kellyDMmendes <kellymendes169@gmail.com>
2024-03-16 22:02:48 -0300

    Create b_1019.py

---
+++ b_1019.py
@@ -0,0 +1,18 @@
+""" b_1019 - Conversão de Tempo """
+HORA = 0
+MINUTO = 0
+SEGUNDO = 0
+n = int(input())
+
+while n > 0:
+    if n >= 3600:
+        HORA += 1
+        n -= 3600
+    elif n >= 60:
+        MINUTO +=1
+        n -= 60
+    else:
+        SEGUNDO += 1
+        n -= 1
+print(f"{HORA}:{MINUTO}:{SEGUNDO}")
+
