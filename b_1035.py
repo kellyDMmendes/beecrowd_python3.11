6565ba8dca3fe74b651d472f6a47a0eeb2b71c3d
kellyDMmendes <kellymendes169@gmail.com>
2024-03-17 15:43:25 -0300

    Create b_1035.py

---
+++ b_1035.py
@@ -0,0 +1,17 @@
+""" b_1035 - Teste de Seleção 1 """
+a, b, c, d = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+d = int(d)
+
+if b > c and d > a:
+    if c + d > a + b and c > 0:
+        if d > 0 and a % 2 == 0:
+            print("Valores aceitos")
+        else:
+            print("Valores nao aceitos")
+    else:
+        print("Valores nao aceitos")
+else:
+    print("Valores nao aceitos")
