6d3148ad54493e31d7f786f8e7b43ecb0ca776be
kellyDMmendes <kellymendes169@gmail.com>
2024-03-17 16:24:15 -0300

    Create b_1038.py

---
+++ b_1038.py
@@ -0,0 +1,16 @@
+""" b_1038 - Lanche """
+cod, quant = input().split()
+cod = int(cod)
+quant = int(quant)
+
+if cod == 1:
+    total = quant * 4
+elif cod == 2:
+    total = quant * 4.5
+elif cod == 3:
+    total = quant * 5
+elif cod == 4:
+    total = quant * 2
+else:
+    total = quant * 1.5
+print(f"Total: R$ {total:.2f}")
