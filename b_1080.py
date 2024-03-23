a8852630389f5c118c0d4e47145d97693b136209
kellyDMmendes <kellymendes169@gmail.com>
2024-03-23 20:56:49 -0300

    Create b_1080.py

---
+++ b_1080.py
@@ -0,0 +1,13 @@
+""" b_1080 - Maior e Posição """
+MAIOR = 0
+CONT = 1
+
+while CONT <= 100:
+    num = int(input())
+    if num > MAIOR:
+        MAIOR = num
+        POS = CONT
+    CONT += 1
+
+print(MAIOR)
+print(POS)
