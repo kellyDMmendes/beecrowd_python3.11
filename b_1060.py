1e6d19bfdb1ff3a917c6b4ebeab6413d57ba25cc
kellyDMmendes <kellymendes169@gmail.com>
2024-03-23 17:36:33 -0300

    Create b_1060.py

---
+++ b_1060.py
@@ -0,0 +1,11 @@
+""" b_1060 - Números Positivos """
+POS = 0
+CONT = 0
+
+while CONT < 6:
+    num = float(input())
+    if num > 0:
+        POS += 1
+    CONT += 1
+
+print(f"{POS} valores positivos")
