876f759485f51bfd1892b9f221aad77a352cba9a
kellyDMmendes <kellymendes169@gmail.com>
2024-03-23 19:49:43 -0300

    Create b_1071.py

---
+++ b_1071.py
@@ -0,0 +1,19 @@
+""" b_1071 - Soma de Ímpares Consecutivos I"""
+IMP = 0
+x = int(input())
+y = int(input())
+
+if x > y:
+    n = x
+    x = y
+    y = n
+
+if x % 2 != 0:
+    x += 1
+
+while x < y:
+    if x % 2 != 0:
+        IMP += x
+    x += 1
+
+print(IMP)
