36593321ba14e3711d03ad6371e757b25d2b3817
kellyDMmendes <kellymendes169@gmail.com>
2024-03-23 18:54:34 -0300

    Create b_1064.py

---
+++ b_1064.py
@@ -0,0 +1,16 @@
+""" b_1064 - Positivos e Médias """
+POS = 0
+MEDIA = 0
+CONT = 0
+
+while CONT < 6:
+    num = float(input())
+    if num > 0:
+        POS += 1
+        MEDIA += num
+    CONT += 1
+
+MEDIA /= POS
+
+print(f"{POS} valores positivos")
+print(f"{MEDIA:.1F}")
