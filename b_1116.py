886d2ce00902a02caedb59446953f703760a245a
kellyDMmendes <kellymendes169@gmail.com>
2024-04-21 15:30:36 -0300

    Create b_1116.py

---
+++ b_1116.py
@@ -0,0 +1,10 @@
+""" b_1116 - Dividindo X por Y """
+n = int(input())
+for i in range(n):
+    x, y = input().split()
+    x = int(x)
+    y = int(y)
+    if y == 0:
+        print("divisao impossivel")
+    else:
+        print(x / y)
