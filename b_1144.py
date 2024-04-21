651b9905c6026d5dd14a52b328df8f210eabc926
kellyDMmendes <kellymendes169@gmail.com>
2024-04-21 17:06:44 -0300

    Create b_1144.py

---
+++ b_1144.py
@@ -0,0 +1,15 @@
+""" b_1144 - Sequência Lógica """
+n = int(input())
+for i in range(1, n+1):
+    x = i
+    print(x, end=" ")
+    y = x * i
+    print(y, end=" ")
+    z = y * i
+    print(z)
+    x = i
+    print(x, end=" ")
+    y += 1
+    print(y, end=" ")
+    z += 1
+    print(z)
