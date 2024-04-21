2aefb312e01eca201b7ce492e4feb92fa4222d36
kellyDMmendes <kellymendes169@gmail.com>
2024-04-21 14:19:52 -0300

    Create b_1099.py

---
+++ b_1099.py
@@ -0,0 +1,14 @@
+""" b_1099 - Soma de Ímpares Consecutivos II """
+IMP = 0
+n = int(input())
+for i in range(n):
+    x, y = input().split()
+    x = int(x)
+    y = int(y)
+    if x > y:
+        x, y = y, x
+    for j in range(x+1, y):
+        if j % 2 != 0:
+            IMP += j
+    print(IMP)
+    IMP = 0
