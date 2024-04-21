d9a9d6f1d0e1470d4cefc8ae118c33cccb2b82d4
kellyDMmendes <kellymendes169@gmail.com>
2024-04-21 15:58:48 -0300

    Create b_1118.py

---
+++ b_1118.py
@@ -0,0 +1,20 @@
+""" b_1118 - Várias Notas Com Validação """
+CONT = 0
+MEDIA = 0
+NOVO = 1
+while NOVO == 1:
+    while CONT < 2:
+        nota = float(input())
+        while nota < 0 or nota > 10:
+            print("nota invalida")
+            nota = float(input())
+        MEDIA += nota
+        CONT += 1
+    print(f"media = {(MEDIA / 2):.2f}")
+    MEDIA = 0
+    CONT = 0
+    print("novo calculo (1-sim 2-nao)")
+    NOVO = int(input())
+    if NOVO != 1 and NOVO != 2:
+        print("novo calculo (1-sim 2-nao)")
+        NOVO = int(input())
