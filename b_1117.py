8905ae39e2d0027d8859a526fe3a9f63b1d4c6d0
kellyDMmendes <kellymendes169@gmail.com>
2024-04-21 15:40:24 -0300

    Create b_1117.py

---
+++ b_1117.py
@@ -0,0 +1,11 @@
+""" b_1117 - Validação de Nota """
+MEDIA = 0
+CONT = 0
+while CONT < 2:
+    nota = float(input())
+    while nota < 0 or nota > 10:
+        print("nota invalida")
+        nota = float(input())
+    MEDIA += nota
+    CONT += 1
+print(f"media = {(MEDIA / 2):.2F}")
