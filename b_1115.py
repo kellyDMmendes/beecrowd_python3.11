a5bad7dc5cd61bafd6a94cb6c2e1099cce3df86d
kellyDMmendes <kellymendes169@gmail.com>
2024-04-21 15:23:47 -0300

    Create b_1115.py

---
+++ b_1115.py
@@ -0,0 +1,16 @@
+""" b_1115 - Quadrante """
+x, y = input().split()
+x = int(x)
+y = int(y)
+while x != 0 and y != 0:
+    if x > 0 and y > 0:
+        print("primeiro")
+    elif x < 0 and y > 0:
+        print("segundo")
+    elif x < 0 and y < 0:
+        print("terceiro")
+    else:
+        print("quarto")
+    x, y = input().split()
+    x = int(x)
+    y = int(y)
